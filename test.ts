4d0e5324dca0f63ca35876f1266b587dd1af9e00
maiembedded-arch <mai.embedded@gmail.com>
2026-04-28 15:31:14 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+serial.setBaudRate(BaudRate.BaudRate115200)
+
+heartRate.start()
+
+forever(function () {
+    heartRate.serialOutput()
+    pause(20)
+})
